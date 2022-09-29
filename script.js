1e8ea9b22b209b2821a52f03318a3731f82157b3
MrLuckyCat <joewilliamdd31@gmail.com>
2022-09-29 08:37:06 +0100

    Add a function to play a single round

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 function main() {
-  const computerChoice = getComputerChoice();
-  const playerChoice = prompt("What's your choice: ", "ROCK").toLowerCase();
-  console.log(playerChoice);
+  let computerChoice = getComputerChoice();
+  let playerChoice = prompt("What's your choice: ", "ROCK").toLowerCase();
+  console.log(playRound(playerChoice, computerChoice));
 }
 
 // ! computer choice
 function getComputerChoice() {
-  let choices = ["ROCK", "PAPER", "SCISSORS"];
+  let choices = ["rock", "paper", "scissors"];
   let choice = choices[Math.floor(Math.random() * 3)];
 
   return choice;
+}
+
+// ! play a round
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "Oh no, you lost this round :(";
+    } else if (computerChoice === "rock") {
+      return "tie!";
+    } else {
+      return "Hooray!!! you won this round!";
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "paper") {
+      return "tie!";
+    } else if (computerChoice === "rock") {
+      return "Hooray!!! you won this round";
+    } else {
+      return "Oh no, you lost this round :(";
+    }
+  } else {
+    if (computerChoice === "paper") {
+      return "Hooray!!! you won this round!";
+    } else if (computerChoice === "rock") {
+      return "Oh no, you lost this round :(";
+    } else {
+      return "tie!";
+    }
+  }
 }
 
 main();
