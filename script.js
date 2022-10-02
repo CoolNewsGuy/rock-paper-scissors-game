e89f23c6361c8da714cbf20fc800d171cf5a8eee
MrLuckyCat <joewilliamdd31@gmail.com>
2022-10-02 13:27:47 +0100

    Add an animation to hide container1 and show container2

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let playBtn = document.getElementById("play");
+let container1 = document.getElementById("container-1");
+let container2 = document.getElementById("container-2");
+
+playBtn.addEventListener("click", hideContainer1, { once: true });
+
+function hideContainer1() {
+    playBtn.style.cursor = "wait";
+    container1.classList.add("fade-out");
+    setTimeout(() => {
+        container1.style.display = "none";
+        showContainer2();
+    }, 2000);
+}
+
+function showContainer2() {
+    container2.style.display = "none";
+    container2.classList.add("fade-in");
+}
 // computer choice
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
